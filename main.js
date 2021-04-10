65bf8130fd2201a33e43c6d5968ae1cd70840b6b
Hao <74541600+haod98@users.noreply.github.com>
2021-04-10 10:41:32 +0200

    Change to switch

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const client = new Discord.Client();
 
 
 
+
+
 client.once('ready', () => console.log('Random bot is online'));
 
 client.on('message', message => {
@@ -14,97 +16,88 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
 
-    /*     if (command === 'help') {
-            const help_embed = new Discord.MessageEmbed().setColor('#0099ff');
-    
-           channel.send(help_embed);
-    
-    
-                    message.channel.send(`Try one of the following commands: \n\
-            ${"`!me`"} Info about yourself \n\
-            ${"`!ghibli help`"} Info about Ghibli movies\n\
-            ${"`!avater @user`"} To get the users profile picture`);
-        }; */
+    if (command === 'help') {
 
-    if (command === 'me') {
-        message.channel.send(`Your Username is: ${message.author.username} \nYour ID is: ${message.author.id}`);
-    };
 
-    if (command === 'test') {
-        const first_msg = message.content;
-        message.channel.send(`The first message ${first_msg}`);
-    };
+        /*         message.channel.send({
+                    embed: {
+                        color: 3447003,
+                        description: "A very simple Embed!"
+                    }
+                }); */
 
-    if (command === 'avatar') {
-        if (!message.mentions.users.size) {
-            console.log(message.mentions.users.size, !message.mentions.users.size);
-            return message.reply(`You need to tag someone`);
-        } else {
-            const tagged_user_id = message.mentions.users.toJSON()[0].id;
-            const tagged_user_avatar = message.mentions.users.toJSON()[0].avatar;
-            const tagged_user = message.mentions.users.first();
-            console.log(!message.mentions.users.size);
-            message.channel.send(`Here is your image from ${tagged_user} https://cdn.discordapp.com/avatars/${tagged_user_id}/${tagged_user_avatar}.png`);
-        };
-    };
-
-    if (command === 'cat') {
-        if (!args.length) {
-            return message.channel.send(`Try using ${"`!cat help`"}`);
-        } else if (args[0] === 'fact') {
-            return fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1').then(response => response.json()).then(data => {
-                message.channel.send(data.text);
-            });
-        } else if (args[0] === 'img') {
-            return fetch(`https://api.thecatapi.com/v1/images/search?api_key=${cat_token}`).then(response => response.json()).then(data => {
-                message.channel.send(data[0].url);
-                if (Object.keys(data[0].breeds).length !== 0) {
-                    message.channel.send(`This cat breed is called: **${data[0].breeds[0].name}**`);
-                };
-            });
-        } else if (args[0] === 'help') {
-            return message.channel.send(`${"`!cat facts`"} for random cat facts\n${"`!cat img`"} for random cat images`);
-        };
-        message.channel.send(`No argument for **${args[0]}**. Try using ${"`!cat help`"}`);
-    };
-
-    if (command === 'dog') {
-        if (!args.length) {
-            return message.channel.send(`Try using ${"`!dog help`"}`);
-        } else if (args[0] === 'fact') {
-            return fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=1').then(response => response.json()).then(data => {
-                message.channel.send(data.text);
-            });
-        } else if (args[0] === 'img') {
-            return fetch(`https://api.thedogapi.com/v1/images/search?api_key=${dog_token}`).then(response => response.json()).then(data => {
-                message.channel.send(data[0].url);
-                if (Object.keys(data[0].breeds).length !== 0) {
-                    message.channel.send(`This dog breed is called: **${data[0].breeds[0].name}**`);
-                };
-            });
-        } else if (args[0] === 'help') {
-            return message.channel.send(`${"`!dog facts`"} for random cat facts\n${"`!dog img`"} for random dog images`)
-        }
-
-        message.channel.send(`No argument for **${args[0]}**. Try using ${"`!cat help`"}`);
+        /*         message.channel.send(`Try one of the following commands: \n\
+                    ${"`!me`"} Info about yourself \n\
+                    ${"`!ghibli help`"} Info about Ghibli movies\n\
+                    ${"`!avater @user`"} To get the users profile picture`);
+                    */
     };
 
 
+    switch (command) {
+        case 'me':
+            message.channel.send(`Your Username is: **${message.author.username}** \nYour ID is: **${message.author.id}**`);
+            break;
+        case 'avatar':
+            if (!message.mentions.users.size) {
+                return message.reply(`You need to tag someone`);
+            } else {
+                const tagged_user_id = message.mentions.users.toJSON()[0].id;
+                const tagged_user_avatar = message.mentions.users.toJSON()[0].avatar;
+                const tagged_user = message.mentions.users.first();
+                message.channel.send(`Here is your image from ${tagged_user} https://cdn.discordapp.com/avatars/${tagged_user_id}/${tagged_user_avatar}.png`);
+            };
+            break;
+        case 'cat':
+            if (!args.length) {
+                return message.channel.send(`Try using ${"`!cat help`"}`);
+            } else if (args[0] === 'fact') {
+                return fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1').then(response => response.json()).then(data => {
+                    message.channel.send(data.text);
+                });
+            } else if (args[0] === 'img') {
+                return fetch(`https://api.thecatapi.com/v1/images/search?api_key=${cat_token}`).then(response => response.json()).then(data => {
+                    message.channel.send(data[0].url);
+                    if (Object.keys(data[0].breeds).length !== 0) {
+                        message.channel.send(`This cat breed is called: **${data[0].breeds[0].name}**`);
+                    };
+                });
+            } else if (args[0] === 'help') {
+                return message.channel.send(`${"`!cat facts`"} for random cat facts\n${"`!cat img`"} for random cat images`);
+            };
+            message.channel.send(`No argument for **${args[0]}**. Try using ${"`!cat help`"}`);
+            break;
+        case 'dog':
+            if (!args.length) {
+                return message.channel.send(`Try using ${"`!dog help`"}`);
+            } else if (args[0] === 'fact') {
+                return fetch('https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=1').then(response => response.json()).then(data => {
+                    message.channel.send(data.text);
+                });
+            } else if (args[0] === 'img') {
+                return fetch(`https://api.thedogapi.com/v1/images/search?api_key=${dog_token}`).then(response => response.json()).then(data => {
+                    message.channel.send(data[0].url);
+                    if (Object.keys(data[0].breeds).length !== 0) {
+                        message.channel.send(`This dog breed is called: **${data[0].breeds[0].name}**`);
+                    };
+                });
+            } else if (args[0] === 'help') {
+                return message.channel.send(`${"`!dog facts`"} for random cat facts\n${"`!dog img`"} for random dog images`)
+            }
 
-    if (command === 'ghibli') {
-        if (!args.length) {
-            return message.channel.send(`Try using ${"`!ghibli help`"}`)
-        } else if (args[0] === 'castle') {
-            return fetch('https://ghibliapi.herokuapp.com/films').then(response => response.json()).then(data => {
-                message.channel.send(data[0].title);
-            });
-        };
-
-        message.channel.send(`No argument for **${args[0]}**. Try using ${"`!ghibli help`"}`);
-
-
+            message.channel.send(`No argument for **${args[0]}**. Try using ${"`!cat help`"}`);
+            break;
+        case 'ghibli':
+            if (!args.length) {
+                return message.channel.send(`Try using ${"`!ghibli help`"}`)
+            } else if (args[0] === 'castle') {
+                return fetch('https://ghibliapi.herokuapp.com/films').then(response => response.json()).then(data => {
+                    message.channel.send(data[0].title);
+                });
+            };
+            message.channel.send(`No argument for **${args[0]}**. Try using ${"`!ghibli help`"}`);
+            break;
     };
-
 });
 
 
