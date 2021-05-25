7c8a5e3130023b06d967fdd45c295c08ce92de92
myin <minyin142p@gmail.com>
2021-05-25 21:17:07 +0200

    Ping server itself

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const request = require("request");
 const express = require("express");
 const app = express();
 const port = process.env.PORT;
@@ -5,8 +6,13 @@ const port = process.env.PORT;
 // Heroku needs a server to bind the port to, otherwise it fails with an error
 app.get("/", (req, res) => {
   res.send("Hello World!");
+  console.log("Called");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+setInterval(() => {
+  request(`http://localhost:${port}`);
+}, 1000 * 60 * 30);
